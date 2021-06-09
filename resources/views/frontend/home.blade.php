9b62b717e67fa11711187d8bf3ca0d380bf110b4
Tejas Soni <tejas.vnerds@gmail.com>
2021-06-09 17:50:48 +0530

    frontend changes

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -34,13 +34,13 @@
     <div class="filter">
         <div class="post nav" id="nav-tab" role="tablist">
             <a class="button btngreen" id="nav-filter-latest-tab" data-bs-toggle="tab" href="#nav-filter-latest"
-                role="tab" aria-controls="nav-filter-latest" aria-selected="false">Latest</a>
+                role="tab" aria-controls="nav-filter-latest" aria-selected="false" onclick="filterPopularPosts()">Latest</a>
             <a class="button btnyellow {{request()->get('p') == 1 ? 'text-dark' : ''}}" id="filter-popular-tab"
                 data-bs-toggle="tab" href="#nav-filter-popular" role="tab" aria-controls="nav-filter-popular"
                 aria-selected="false" onclick="filterPopularPosts()">Popular</a>
             <input type="hidden" name="popular" id="popular" value="{{request()->get('p') ?? 0}}" />
             <a class="button btndarkyellow" id="filter-trending-tab" data-bs-toggle="tab" href="#nav-filter-trending"
-                role="tab" aria-controls="nav-filter-trending" aria-selected="false">Trending</a>
+                role="tab" aria-controls="nav-filter-trending" aria-selected="false" onclick="filterPopularPosts()">Trending</a>
         </div>
         <div class="artsoption">
             <div class="commissions">
